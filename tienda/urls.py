2a4ea795ef587d0a805641821999ff8b6021af83
unknown <Marisikamiar@gmail.com>
2021-11-29 02:32:41 +0100

    actualizando datos

--- tienda/urls.py
+++ tienda/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
 
-    path('',views.tienda,name="tienda"),
-   
+    path('',views.tienda, name="tienda"),
+    
 ]
--- tienda/views.py
+++ tienda/views.py
@@ -1,5 +1,8 @@
+from django.views import generic
+from django.db.models import query_utils
 from django.shortcuts import render
 from .models import Producto
+from django.db.models import Q
 
 
 # Create your views here.
@@ -8,3 +11,21 @@ def tienda (request):
     productos=Producto.objects.all()
     return render (request, "tienda/tienda.html",{"productos":productos})
 
+
+
+class ProductoListView(generic.ListView):
+    '''Vista genérica para nuestro listado de productos'''
+    model = Producto
+    paginate_by = 15
+    queryset = Producto.objects.all().order_by('nombre')
+
+
+class SearchResultsListView():
+    model = Producto
+    context_object_name ='producto_list'
+ 
+    def get_queryset(self): 
+       
+        query = self.request.GET.get('q')
+        q1 = Q(nombre_producto_icontains = query)
+        return Producto.objects.filter(q1)
